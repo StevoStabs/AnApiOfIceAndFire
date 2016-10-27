13712e39df764ebd52c8e1269990b1a557a9d688
joakimskoog <joakimskooog@gmail.com>
2016-10-27 20:28:53 +0200

    Started to work on some css

--- src/AnApiOfIceAndFire/wwwroot/js/site.js
+++ src/AnApiOfIceAndFire/wwwroot/js/site.js
@@ -1,1 +1,40 @@
-﻿// Write your Javascript code.
+﻿function makeApiRequest(url) {
+    if (url == '') { //User submitted empty url or did not change the placeholder text.
+        url = 'characters/583';
+    }
+
+    url = 'https://anapioficeandfire.com/api/' + url;
+
+    $.ajax({
+        url: url,
+        context: document.body
+    }).complete(function (data) {
+        if (data['status'] == 200) {
+            var jsonData = $.parseJSON(data['responseText']);
+            $('#responseOutput').text(JSON.stringify(jsonData, null, '\t'));
+        }
+        else if (data['status'] == 404) {
+            $('#responseOutput').text(data['status'] + ' ' + data['statusText']);
+        } else {
+            $('#responseOutput').text('Something went wrong!');
+        }
+    });
+}
+
+
+$('#btnMakeApiRequest').click(function () {
+    var url = $('#btnRequestUrl').val();
+    makeApiRequest(url);
+});
+
+$('.request-suggestion').click(function (e) {
+    e.preventDefault();
+    var url = e.target.innerText;
+    $('#btnRequestUrl').val(url);
+    makeApiRequest(url);
+});
+
+$(function () {
+    var url = $('#btnRequestUrl').val();
+    makeApiRequest(url);
+});
